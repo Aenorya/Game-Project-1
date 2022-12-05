c49e12ebf3d4195cdb90e6865062868b3d03a1d7
Wolf Rex <80474693+WolfRex7420@users.noreply.github.com>
2022-12-05 15:15:49 +0100

    Loris First commit

---
+++ Assets/Loris/Scripts/Copie de LoadSpecificScene.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LoadSpecificScene : MonoBehaviour
+{
+    public string sceneName;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(sceneName);
+            //LoadAndSaveData.instance.SaveData();
+        }
+    }
+}
---
+++ Assets/Loris/Scripts/Copie de PlayerHealth.cs
@@ -0,0 +1,117 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public int maxHealth = 100;
+    public int currentHealth;
+
+    public float invincibilityTimeAfterHit = 3f;
+    public float invincibilityFlashDelay = 0.2f;
+    public bool isInvincible = false;
+
+    public SpriteRenderer graphics;
+    public HealthBar healthBar;
+
+    public AudioClip hitSound;
+
+    public static PlayerHealth instance;
+
+    private void Awake()
+    {
+        if (instance != null)
+        {
+            Debug.LogWarning("Il y a plus d'une instance de PlayerHealth dans la scène");
+            return;
+        }
+
+        instance = this;
+    }
+
+    void Start()
+    {
+        currentHealth = maxHealth;
+        healthBar.SetMaxHealth(maxHealth);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.H))
+        {
+            TakeDamage(25);
+        }
+    }
+
+    public void HealPlayer(int amount)
+    {
+        if((currentHealth + amount) > maxHealth)
+        {
+            currentHealth = maxHealth;
+        }
+        else
+        {
+            currentHealth += amount;
+        }
+
+        healthBar.SetHealth(currentHealth);
+    }
+
+    public void TakeDamage(int damage)
+    {
+        if (!isInvincible)
+        {
+            //AudioManager.instance.PlayClipAt(hitSound, transform.position);
+            currentHealth -= damage;
+            healthBar.SetHealth(currentHealth);
+
+            if(currentHealth <= 0)
+            {
+                Die();
+                return;
+            }
+
+            isInvincible = true;
+            StartCoroutine(InvincibilityFlash());
+            StartCoroutine(HandleInvincibilityDelay());
+        }
+    }
+
+    public void Die()
+    {
+        PlayerScript.instance.enabled = false;
+        PlayerScript.instance.animator.SetTrigger("Death");
+        PlayerScript.instance.rb.bodyType = RigidbodyType2D.Kinematic;
+        PlayerScript.instance.rb.velocity = Vector3.zero;
+        PlayerScript.instance.playerCollider.enabled = false;
+        GameOverManager.instance.OnPlayerDeath();
+        Debug.Log("Player eliminated");
+    }
+
+   /* public void Respawn()
+    {
+        PlayerScript.instance.enabled = true;
+        PlayerScript.instance.animator.SetTrigger("Respawn");
+        PlayerScript.instance.rb.bodyType = RigidbodyType2D.Dynamic;
+        PlayerScript.instance.playerCollider.enabled = true;
+        currentHealth = maxHealth;
+        healthBar.SetHealth(currentHealth);
+    }*/
+
+    public IEnumerator InvincibilityFlash()
+    {
+        while (isInvincible)
+        {
+            graphics.color = new Color(1f, 1f, 1f, 0f);
+            yield return new WaitForSeconds(invincibilityFlashDelay);
+            graphics.color = new Color(1f, 1f, 1f, 1f);
+            yield return new WaitForSeconds(invincibilityFlashDelay);
+        }
+    }
+
+    public IEnumerator HandleInvincibilityDelay()
+    {
+        yield return new WaitForSeconds(invincibilityTimeAfterHit);
+        isInvincible = false;
+    }
+}

b1250aff86a8ae06a50e3e9faf8307cb73f0187d
Wolf Rex <80474693+WolfRex7420@users.noreply.github.com>
2022-12-05 15:38:32 +0100

    Loris testing mouv

---
+++ Assets/Loris/Scripts/CharaControls.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CharaControls : MonoBehaviour
+{
+    [SerializeField] LayerMask groundLayer;
+    [SerializeField] Rigidbody2D rb;
+    [SerializeField] Transform Bottom;
+    bool isGrounded;
+    float mx;
+    float jumpCoolDown;
+    public Vector3 jump;
+
+    public float moveSpeed = 7f;
+    public float jumpHeight = 2.0f;
+
+    public static CharaControls instance;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        jump = new Vector3(0.0f, 2.0f, 0.0f);
+    }
+
+    void OnCollisionStay()
+    {
+        isGrounded = true;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+
+            //rb.AddForce(jump * jumpHeight, ForceMode.Impulse);
+            isGrounded = false;
+        }
+        mx = Input.GetAxis("Horizontal");
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            SceneManager.LoadScene("MainMenu");
+        }
+
+    }
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(mx * moveSpeed, rb.velocity.y);
+    }
+
+}
---
+++ Assets/Loris/Scripts/DeathZone.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class DeathZone : MonoBehaviour
+{
+
+    private Transform playerSpawn;
+
+    private void Awake()
+    {
+        playerSpawn = GameObject.FindGameObjectWithTag("Respawn").transform;
+    }
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            collision.transform.position = playerSpawn.position;
+        }
+    }
+}

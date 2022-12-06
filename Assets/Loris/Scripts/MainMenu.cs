6246b4a0bb2742d9cdfc14708e08047766ac9971
Wolf Rex <80474693+WolfRex7420@users.noreply.github.com>
2022-12-06 10:32:43 +0100

    Loris Main Menu & Credits & SoundEffects

---
+++ Assets/Loris/Scripts/MainMenu.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+
+    public GameObject settingsWindow;
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Loris Test");
+    }
+
+    public void SettingsButton()
+    {
+        settingsWindow.SetActive(true);
+    }
+
+    public void CloseSettingsWindow()
+    {
+        settingsWindow.SetActive(false);
+    }
+
+    public void LoadCreditsScene()
+    {
+        SceneManager.LoadScene("Credits");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void TutorialScene()
+    {
+        SceneManager.LoadScene("TutorialScene");
+    }
+}

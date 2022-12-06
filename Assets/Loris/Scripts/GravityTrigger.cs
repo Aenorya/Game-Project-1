439407ca3d6c49c28e71b25defdf0cf753c23aaf
Wolf Rex <80474693+WolfRex7420@users.noreply.github.com>
2022-12-06 11:07:44 +0100

    Loris awawa

--- Assets/Loris/Scripts/GravityTrigger.cs
+++ Assets/Loris/Scripts/GravityTrigger.cs
@@ -5,10 +5,11 @@ using UnityEngine;
 public class GravityTrigger : MonoBehaviour
 {
     public Rigidbody2D rb;
+    public int grav;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        rb.gravityScale = 0.25f;
+        rb.gravityScale = grav;
     }
     private void OnTriggerExit2D(Collider2D collision)
     {

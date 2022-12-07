09ad18d646389a84b8427171bcb200ccdbe02540
Wolf Rex <80474693+WolfRex7420@users.noreply.github.com>
2022-12-07 17:44:43 +0100

    Loris Camera Follow

--- Assets/Loris/Scripts/CameraFollow.cs
+++ Assets/Loris/Scripts/CameraFollow.cs
@@ -11,7 +11,5 @@ public class CameraFollow : MonoBehaviour
     void Update()
     {
         transform.position = Vector3.SmoothDamp(transform.position, player.transform.position + posOffset, ref velocity, timeOffset);
-
-
     }
 }

264b49ebd50085fb88f6c4f948637ed721f51478
ZoeChrvx <zchervaux@artfx.fr>
2022-12-08 11:22:03 +0100

    Update Zoe ok

--- Assets/Zoe/Script/PlayerController.cs
+++ Assets/Zoe/Script/PlayerController.cs
@@ -132,17 +132,18 @@ public class PlayerController : MonoBehaviour
         }
     }
 
-    private void OnCollisionEnter2D(Collision pickObjCollision)
-    {
-        if (pickObjCollision.gameObject.CompareTag("PickableObject"))
-        {
-            Destroy(pickObjCollision.gameObject);
-
-        }
-    }
-
     private void OnCollisionEnter2D(Collision2D collision)
     {
         isGrounded = true;
+        if (collision.gameObject.CompareTag("PickableObject"))
+        {
+            Debug.Log("Seringue");
+            Destroy(collision.gameObject);
+        }
+    }
+
+    public void PickUpSyringe()
+    {
+
     }
 }

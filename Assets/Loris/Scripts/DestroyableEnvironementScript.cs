ebc3021a9d12ffef7f80d25b659a430ab639f8e9
Wolf Rex <80474693+WolfRex7420@users.noreply.github.com>
2022-12-08 09:17:30 +0100

    Loris

---
+++ Assets/Loris/Scripts/DestroyableEnvironementScript.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyableEnvironementScript : MonoBehaviour
+{
+    public GameObject Destroyable;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Destroy(Destroyable);
+        }
+    }
+}

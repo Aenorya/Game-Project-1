e35f7166583f60396bb8eb9650b97c94615b35c2
Wolf Rex <80474693+WolfRex7420@users.noreply.github.com>
2022-12-13 13:39:55 +0100

    Loris Enemy v2 update (again)

--- Assets/Zoe/Script/Collision.cs
+++ Assets/Zoe/Script/Collision.cs
@@ -6,11 +6,12 @@ public class Collision : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.gameObject.CompareTag("Enemy"))
+        /*if(collision.gameObject.CompareTag("Enemy"))
         {
             EnemyScriptTest.instance.TakeDamage(PlayerController.damage);
             //Debug.Log(EnemyScriptTest.health);
-        }
+        }*/
+
         if (collision.gameObject.CompareTag("ReBox"))
         {
             BoxDrop.instance.HitBox(PlayerController.damage);

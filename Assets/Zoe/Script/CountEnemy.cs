dca95084331f46a817f1de86b085ede21c5c0130
ZoeChrvx <zchervaux@artfx.fr>
2022-12-14 16:46:47 +0100

    Zoz Canvas

---
+++ Assets/Zoe/Script/CountEnemy.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class CountEnemy : MonoBehaviour
+{
+    public int numberEnemy;
+    public TextMeshProUGUI textNbEnemy;
+
+    public static CountEnemy instance;
+
+    public void Awake()
+    {
+        instance = this;
+    }
+    
+    void Update()
+    {
+        textNbEnemy.text = "Enemy remaining : " + numberEnemy;
+    }
+}

e13f2cc32538dcff07b2df2b4dc4b0b13dc01157
Wolf Rex <80474693+WolfRex7420@users.noreply.github.com>
2022-12-15 09:54:48 +0100

    Loris

--- Assets/Loris/Enemy_behaviour.cs
+++ Assets/Loris/Enemy_behaviour.cs
@@ -208,9 +208,9 @@ public class Enemy_behaviour : MonoBehaviour
         transform.eulerAngles = rotation;
     }
 
-    private void OnDrawGizmos()
+    /*private void OnDrawGizmos()
     {
         Gizmos.color = Color.green;
         Gizmos.DrawWireSphere(transform.position, attackDistance);
-    }
+    }*/
 }

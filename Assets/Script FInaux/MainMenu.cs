b20307ff6304019acfed9d94bc9cfd527d15acc4
Wolf Rex <80474693+WolfRex7420@users.noreply.github.com>
2023-01-05 16:59:01 +0100

    Updates menus

--- Assets/Script FInaux/MainMenu.cs
+++ Assets/Script FInaux/MainMenu.cs
@@ -8,7 +8,7 @@ public class MainMenu : MonoBehaviour
 
     public void StartGame()
     {
-        SceneManager.LoadScene("Asset Testing");
+        SceneManager.LoadScene("Level_01");
     }
 
     public void SettingsButton()

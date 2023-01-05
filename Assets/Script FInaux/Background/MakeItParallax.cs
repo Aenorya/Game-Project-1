f19bbe5c0a971a88f13c1529f93743816ec197c8
alanrostami <alan.rostami@artfx.fr>
2023-01-05 12:18:58 +0100

    Alan [modify] parallax script

--- Assets/Script FInaux/Background/MakeItParallax.cs
+++ Assets/Script FInaux/Background/MakeItParallax.cs
@@ -8,7 +8,7 @@ public class MakeItParallax : MonoBehaviour
     public Camera sceneCamera;
 
     // Reference for player (Subject)
-    public Transform player;
+    public Transform scenePlayer;
 
     Vector2 startPosition;
     float startZ;
@@ -16,20 +16,20 @@ public class MakeItParallax : MonoBehaviour
     // Distance that camera has moved from the original position of the sprite
     Vector2 travel => (Vector2)sceneCamera.transform.position - startPosition;
 
-    float distanceFromSubject => transform.position.z - player.position.z;
+    float distanceFromSubject => transform.position.z - scenePlayer.position.z;
     float clippingPlane => (sceneCamera.transform.position.z + (distanceFromSubject > 0 ? sceneCamera.farClipPlane : sceneCamera.nearClipPlane));
 
     float parallaxFactor => Mathf.Abs(distanceFromSubject) / clippingPlane;
 
     // Vector2 parallaxFactor;
 
-    public void Start()
+    void Start()
     {
         startPosition = transform.position;
         startZ = transform.position.z;
     }
 
-    public void Update()
+    void Update()
     {
         Vector2 newPos = startPosition + travel * parallaxFactor;
         transform.position = new Vector3(newPos.x, newPos.y, startZ);
